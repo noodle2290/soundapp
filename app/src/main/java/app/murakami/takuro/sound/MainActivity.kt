e44e7984ccd76feadedfbd846a5034ae9de12f19
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-05-13 05:07:24 +0900

    sound1 complete

---
+++ app/src/main/java/app/murakami/takuro/sound/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.murakami.takuro.sound
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
+
+        drumImage.setOnClickListener{
+
+            drumSound.seekTo(0)
+
+            drumSound.start()
+        }
+    }
+}
