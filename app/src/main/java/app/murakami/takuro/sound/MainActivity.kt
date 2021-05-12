75d4e3afac7eda1a7f39ac7dc80ffeff22cf4eec
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-05-13 05:26:21 +0900

    サウンドアプリ２完成

--- app/src/main/java/app/murakami/takuro/sound/MainActivity.kt
+++ app/src/main/java/app/murakami/takuro/sound/MainActivity.kt
@@ -3,6 +3,7 @@ package app.murakami.takuro.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,21 @@ class MainActivity : AppCompatActivity() {
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener{
+        drumImage.setOnTouchListener { view, event ->
 
-            drumSound.seekTo(0)
+            if(event.action == MotionEvent.ACTION_DOWN) {
 
-            drumSound.start()
+                drumImage.setImageResource((R.drawable.drum_playing_image))
+
+                drumSound.seekTo(0)
+                drumSound.start()
+            }
+
+            else if (event.action == MotionEvent.ACTION_UP){
+
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+            true
         }
     }
 }
